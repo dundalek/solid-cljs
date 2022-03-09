99d91c4edc21b92567b1127f7106a7ce6e221687
Jakub Dundalek <dundalek@gmail.com>
2022-03-09 20:46:21 +0100

    Experiment with proxy wrapping

--- src/solid/app.cljs
+++ src/solid/app.cljs
@@ -1,12 +1,36 @@
 (ns solid.app
   (:require
+   ["solid-js" :refer [createSignal]]
    ["solid-js/web" :refer [render]]
-   [solid.core :refer [defui $]]
+   [solid.core :refer [defui defui2 $]]
    [solid.todos :as todos]
    [solid.tutorial :as tutorial]))
 
+(defui2 counter [{:keys [size set-size add-ten]}]
+  ($ :div
+    ($ :button
+      {:onClick #(set-size inc)}
+      "Child Add")
+    ($ :button
+      {:onClick add-ten}
+      "Child Add Ten")
+    ($ :div size)
+    ($ :div #(* (size) 2))))
+
+(defui demo []
+  (let [[size set-size] (createSignal 10)]
+    ($ :<>
+      ($ :button {:onClick #(set-size inc)} "Parent Add")
+      ($ counter {:size 100
+                  :set-size #()
+                  :add-ten #()})
+      ($ counter {:size size
+                  :set-size set-size
+                  :add-ten #(set-size (+ (size) 10))}))))
+
 (defui app []
   ($ :<>
+    ($ demo)
     ($ todos/main)
     ($ tutorial/main)))
 
--- src/solid/core.cljs
+++ src/solid/core.cljs
@@ -1,5 +1,15 @@
 (ns solid.core
-  (:require ["solid-js/h" :as hyperscript])
+  (:require ["solid-js/h" :as hyperscript]
+            [cljs-bean.core]
+            [goog.object :as gobj])
   (:require-macros [solid.core]))
 
 (def h hyperscript)
+
+(def proxy-props-handler
+  #js {:get (fn [target prop]
+              (fn [& args]
+                (let [x (gobj/get target prop)]
+                  (if (fn? x)
+                    (apply x args)
+                    x))))})
